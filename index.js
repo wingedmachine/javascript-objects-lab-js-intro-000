7f087fac5cc62e9254be3449146737950a053e8f
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 19:06:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, {key:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
